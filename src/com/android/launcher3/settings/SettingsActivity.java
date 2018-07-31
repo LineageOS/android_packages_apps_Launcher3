8fa46261999e182b474a38d8ec5473514b02c2ca
Edward Cross <edward.k.cross@gmail.com>
2018-07-31 15:17:57 +0200

    QuickStep: Google Feed integration
    If Google Search is installed, the Google Feed can be found at
    the -1 screen (which is the very left).
    This feature is enabled by default.
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: I495196818699fd378cd60e6dd61b07a0ab951762

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -16,6 +16,7 @@
 
 package com.android.launcher3.settings;
 
+import static android.os.Process.myUserHandle;
 import static android.provider.Settings.Global.DEVELOPMENT_SETTINGS_ENABLED;
 
 import static androidx.preference.PreferenceFragmentCompat.ARG_PREFERENCE_ROOT;
@@ -28,6 +29,7 @@ import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.LauncherApps;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
@@ -85,6 +87,9 @@ public class SettingsActivity extends FragmentActivity
 
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
+
+    private static final String KEY_MINUS_ONE = "pref_enable_minus_one";
+    private static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -313,6 +318,7 @@ public class SettingsActivity extends FragmentActivity
          * will remove that preference from the list.
          */
         protected boolean initPreference(Preference preference) {
+            LauncherApps launcherApps = getContext().getSystemService(LauncherApps.class);
             LauncherDisplayInfo info = DisplayController.INSTANCE.get(getContext()).getInfo();
             switch (preference.getKey()) {
                 case NOTIFICATION_DOTS_PREFERENCE_KEY:
@@ -342,6 +348,9 @@ public class SettingsActivity extends FragmentActivity
                             }
                     );
                     return !info.isLargeScreen(info.realBounds);
+                case KEY_MINUS_ONE:
+                    return launcherApps != null &&
+                            launcherApps.isPackageEnabled(SEARCH_PACKAGE, myUserHandle());
             }
             return true;
         }
