10d54215281025f622398bc88edf5394c61727e7
Bruno Martins <bgcngm@gmail.com>
2023-10-12 00:31:27 +0100

    SettingsActivity: Hide home screen rotation pref if not supported
    Change-Id: Id697fa2c3021e9f9a723d8828ba7dfe0fae96c2e

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -316,6 +316,11 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                         // Launcher supports rotation by default. No need to show this setting.
                         return false;
                     }
+                    if (!getContext().getResources().getBoolean(
+                            com.android.internal.R.bool.config_supportAutoRotation)) {
+                        // Not supported by the device, hide setting.
+                        return false;
+                    }
                     // Initialize the UI once
                     preference.setDefaultValue(RotationHelper.getAllowRotationDefaultValue(info));
                     return true;
