c2714d210c4097a04f644ecc6c4f4e0b23ad21a0
Ido Ben-Hur <idoybh2@gmail.com>
2024-09-28 13:14:25 +0300

    Launcher3: SwitchPreference -> SwitchPreferenceCompat
    And use the new style
    
    Change-Id: I9bc7ed21d7d3e9ccace738bbd4391d16eb7f6fde

--- quickstep/src/com/android/quickstep/logging/SettingsChangeLogger.java
+++ quickstep/src/com/android/quickstep/logging/SettingsChangeLogger.java
@@ -101,7 +101,7 @@ public class SettingsChangeLogger implements OnSharedPreferenceChangeListener {
             new DaggerSingletonObject<>(QuickstepBaseAppComponent::getSettingsChangeLogger);
 
     private static final String TAG = "SettingsChangeLogger";
-    private static final String BOOLEAN_PREF = "SwitchPreference";
+    private static final String BOOLEAN_PREF = "SwitchPreferenceCompat";
 
     private final Context mContext;
     private final ArrayMap<String, LoggablePref> mLoggablePrefs;
