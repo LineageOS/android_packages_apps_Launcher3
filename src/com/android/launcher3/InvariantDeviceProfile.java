e497dc18957f8e71f1c156498da10b32e5ea6afe
Frederico Silva <fredericojssilva@gmail.com>
2025-01-28 23:43:17 +0000

    Trebuchet: Implement auto open keyboard in drawer setting
    Add setting to always show keyboard when swiping up on your home screen
    
    Change-Id: Ib2cdbcf23d5b46dc44a1641215f20c1ae20c4731

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -119,6 +119,7 @@ public class InvariantDeviceProfile {
     private static final float ICON_SIZE_DEFINED_IN_APP_DP = 48;
 
     public static final String KEY_ALLAPPS_THEMED_ICONS = "pref_allapps_themed_icons";
+    public static final String KEY_DRAWER_OPEN_KEYBOARD = "pref_drawer_open_keyboard";
     public static final String KEY_SHOW_DESKTOP_LABELS = "pref_desktop_show_labels";
     public static final String KEY_SHOW_DRAWER_LABELS = "pref_drawer_show_labels";
     public static final String KEY_WORKSPACE_LOCK = "pref_workspace_lock";
