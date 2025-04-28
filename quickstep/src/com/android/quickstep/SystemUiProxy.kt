1d2fccf875de021e71621d150360462221f17058
LuK1337 <priv.luk@gmail.com>
2025-04-28 16:00:05 +0200

    Launcher3: Rework home button double tap handling
    Instead of injecting double tap, let's just inject single tap and let
    PhoneWindowManager differentiate between the two.
    
    Test: Set home double tap action and notice that using it no longer goes
          home.
    Change-Id: Ic5ba4ca2aa195d7abf8b6d11c91701b1fb6e8833

--- quickstep/src/com/android/quickstep/SystemUiProxy.kt
+++ quickstep/src/com/android/quickstep/SystemUiProxy.kt
@@ -213,9 +213,9 @@ class SystemUiProxy @Inject constructor(@ApplicationContext private val context:
             systemUiProxy?.injectLongPress(keyCode)
         }
 
-    fun injectDoublePress(keyCode: KeyEvent?) =
-        executeWithErrorLog({ "Failed call injectDoublePress" }) {
-            systemUiProxy?.injectDoublePress(keyCode)
+    fun injectPress(keyCode: KeyEvent?) =
+        executeWithErrorLog({ "Failed call injectPress" }) {
+            systemUiProxy?.injectPress(keyCode)
         }
 
     fun onImeSwitcherPressed() =
