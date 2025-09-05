e4deefc46ccbc36fcc80ca59ac7e688a090fb70f
Tony Wickham <twickham@google.com>
2025-09-05 20:42:51 +0000

    Add tests for notification shade via Launcher/Trackpad
    Flag: com.android.systemui.scene_container
    Test: TaplTestsTrackpad
    Test: TaplTestsQuickstep
    Bug: 436813371
    
    Change-Id: I9ea57779ad59d61a002be3926143532c202f2ef6

--- quickstep/tests/src/com/android/quickstep/TaplTestsQuickstep.java
+++ quickstep/tests/src/com/android/quickstep/TaplTestsQuickstep.java
@@ -28,6 +28,7 @@ import static org.junit.Assume.assumeTrue;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.platform.systemui_tapl.ui.Root;
 import android.platform.test.annotations.DisableFlags;
 import android.platform.test.annotations.EnableFlags;
 
@@ -618,6 +619,13 @@ public class TaplTestsQuickstep extends AbstractQuickStepTest {
                     appsInEachRowCount,
                     recentsView.getBottomRowTaskCountForTablet());
         });
+    }
+
+    @Test
+    @NavigationModeSwitch
+    @PortraitLandscape
+    public void testNotificationsFromHome() {
+        Root.get().openNotificationShadeViaSwipe();
     }
 
     private void startTestAppsWithCheck() throws Exception {
