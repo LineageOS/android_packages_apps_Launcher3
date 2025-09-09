28b57f7ada87c83daaad4efbc0e7d603634ecb93
LuK1337 <priv.luk@gmail.com>
2025-09-09 09:36:28 +0200

    Trebuchet: Use HiddenAppsFilter for all apps list
    Change-Id: I1e79577da115f718a1a812acb5fe918d7bdb5fdd

--- src/com/android/launcher3/lineage/trust/HiddenAppsFilter.java
+++ src/com/android/launcher3/lineage/trust/HiddenAppsFilter.java
@@ -18,14 +18,19 @@ package com.android.launcher3.lineage.trust;
 import android.content.ComponentName;
 import android.content.Context;
 
+import com.android.launcher3.dagger.ApplicationContext;
+
 import com.android.launcher3.AppFilter;
 import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
+
+import javax.inject.Inject;
 
 @SuppressWarnings("unused")
 public class HiddenAppsFilter extends AppFilter {
     private TrustDatabaseHelper mDbHelper;
 
-    public HiddenAppsFilter(Context context) {
+    @Inject
+    public HiddenAppsFilter(@ApplicationContext Context context) {
         super(context);
 
         mDbHelper = TrustDatabaseHelper.getInstance(context);
