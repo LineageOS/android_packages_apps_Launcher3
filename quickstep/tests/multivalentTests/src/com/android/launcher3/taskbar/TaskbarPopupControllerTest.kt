eba29a7995568e37ea814f65144f4bff302e21b8
Toni Barzic <tbarzic@google.com>
2025-09-18 04:35:38 +0000

    Handle a11y long click action in taskbar directly
    Updates TaskbarShortcutMenuAccessibilityDelegate to handle long click
    action on icons - both onese tagged by ItemInfo, and ones tagged by a
    GroupTask (i.e. running apps in takbar) by opening the icon options
    menu. This stops the acction from reaching BubbleTextView and starting
    drag interaction in response to a long click. This drag interaction
    would never get cleaned  up given that the view never receives a motion
    event to end the drag.
    
    Bug: b/431155422
    Test: Open and close taskbar options menu using talkback - verify that
          no abandoned drag icons remain when options menus get reopen.
    Flag: EXEMPT BUGFIX
    
    Change-Id: If5f1dd4a747b995ee0a18a16f712fbf694c32f30

--- quickstep/tests/multivalentTests/src/com/android/launcher3/taskbar/TaskbarPopupControllerTest.kt
+++ quickstep/tests/multivalentTests/src/com/android/launcher3/taskbar/TaskbarPopupControllerTest.kt
@@ -22,6 +22,7 @@ import android.platform.test.annotations.DisableFlags
 import android.platform.test.annotations.EnableFlags
 import android.platform.test.flag.junit.SetFlagsRule
 import android.util.SparseArray
+import android.view.accessibility.AccessibilityNodeInfo
 import com.android.launcher3.AbstractFloatingView
 import com.android.launcher3.BubbleTextView
 import com.android.launcher3.Flags.FLAG_ENABLE_MULTI_INSTANCE_MENU_TASKBAR
@@ -41,6 +42,7 @@ import com.android.launcher3.taskbar.rules.TaskbarUnitTestRule.InjectController
 import com.android.launcher3.taskbar.rules.TaskbarWindowSandboxContext
 import com.android.launcher3.util.LauncherMultivalentJUnit
 import com.android.launcher3.util.LauncherMultivalentJUnit.EmulatedDevices
+import com.android.launcher3.util.TestUtil.getOnUiThread
 import com.android.quickstep.util.GroupTask
 import com.android.window.flags.Flags.FLAG_ENABLE_PINNING_APP_WITH_CONTEXT_MENU
 import com.google.common.truth.Truth.assertThat
@@ -117,6 +119,36 @@ class TaskbarPopupControllerTest {
     }
 
     @Test
+    fun showForIconUsingA11yAction_hotseatItem() {
+        assertThat(hasPopupMenu()).isFalse()
+        whenever(desktopVisibilityController.isInDesktopMode(context.displayId)).thenReturn(true)
+
+        runOnMainSync {
+            hotseatIcon.performAccessibilityAction(AccessibilityNodeInfo.ACTION_LONG_CLICK, null)
+        }
+        assertThat(hasPopupMenu()).isTrue()
+        assertThat(hasTaskbarDragView()).isFalse()
+
+        closePopupMenu()
+        assertThat(hasTaskbarDragView()).isFalse()
+    }
+
+    @Test
+    fun showForIconUsingA11yAction_recentTask() {
+        assertThat(hasPopupMenu()).isFalse()
+        whenever(desktopVisibilityController.isInDesktopMode(context.displayId)).thenReturn(true)
+
+        runOnMainSync {
+            recentTaskIcon.performAccessibilityAction(AccessibilityNodeInfo.ACTION_LONG_CLICK, null)
+        }
+        assertThat(hasPopupMenu()).isTrue()
+        assertThat(hasTaskbarDragView()).isFalse()
+
+        closePopupMenu()
+        assertThat(hasTaskbarDragView()).isFalse()
+    }
+
+    @Test
     fun createPinShortcut_itemAlreadyPinned_returnsUnpinShortcut() {
         val hotseatItems = SparseArray<ItemInfo>()
         val appUser = android.os.Process.myUserHandle()
@@ -154,11 +186,29 @@ class TaskbarPopupControllerTest {
         Assert.assertFalse((shortcut as PinToTaskbarShortcut<*>).isPin)
     }
 
+    private fun hasTaskbarDragView(): Boolean {
+        return getOnUiThread {
+            val dragView: TaskbarDragView? =
+                taskbarContext.dragLayer.findViewByPredicate { it is TaskbarDragView }
+            dragView != null
+        }
+    }
+
     private fun hasPopupMenu(): Boolean {
-        return AbstractFloatingView.hasOpenView(
-            taskbarContext,
-            AbstractFloatingView.TYPE_ACTION_POPUP,
-        )
+        return getOnUiThread {
+            AbstractFloatingView.hasOpenView(taskbarContext, AbstractFloatingView.TYPE_ACTION_POPUP)
+        }
+    }
+
+    private fun closePopupMenu() {
+        runOnMainSync {
+            val popup: AbstractFloatingView =
+                AbstractFloatingView.getOpenView(
+                    taskbarContext,
+                    AbstractFloatingView.TYPE_ACTION_POPUP,
+                )
+            popup?.close(false)
+        }
     }
 
     @Test
