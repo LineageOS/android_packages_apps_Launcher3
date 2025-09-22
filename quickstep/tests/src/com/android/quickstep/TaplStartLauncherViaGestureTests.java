9f5b15ca7526dc110c07a0a8de8a49282710aed3
Shamali Patwa <shamalip@google.com>
2025-09-21 21:05:46 -0700

    Add test stability to TaplStartLauncherViaGestureTests intended in ag/35086275
    Fix: 446234327
    Flag: EXEMPT BUGFIX
    Test: Presubmit
    Change-Id: I6d859c44c769ea6db34a2c9f320be28b8428f472

--- quickstep/tests/src/com/android/quickstep/TaplStartLauncherViaGestureTests.java
+++ quickstep/tests/src/com/android/quickstep/TaplStartLauncherViaGestureTests.java
@@ -16,6 +16,7 @@
 
 package com.android.quickstep;
 
+import com.android.launcher3.util.rule.TestStabilityRule;
 import static com.android.launcher3.util.rule.TestStabilityRule.LOCAL;
 import static com.android.launcher3.util.rule.TestStabilityRule.PLATFORM_POSTSUBMIT;
 import static com.android.launcher3.util.rule.TestStabilityRule.Stability;
@@ -28,7 +29,9 @@ import androidx.test.runner.AndroidJUnit4;
 import com.android.quickstep.NavigationModeSwitchRule.NavigationModeSwitch;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 @LargeTest
@@ -42,6 +45,8 @@ public class TaplStartLauncherViaGestureTests extends AbstractQuickStepTest {
     private enum TestCase {
         TO_HOME, TO_OVERVIEW,
     }
+
+    @Rule public TestRule testStabilityRule = new TestStabilityRule();
 
     @Override
     @Before
