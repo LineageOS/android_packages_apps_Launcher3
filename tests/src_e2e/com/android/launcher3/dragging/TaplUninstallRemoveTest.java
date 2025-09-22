ef25348954f6b06556f059a0b7f679bd370fd85f
Schneider Victor-tulias <victortulias@google.com>
2025-09-22 08:21:10 -0700

    Revert "Add screen recording to TaplUninstallRemoveTest.uninstallWorkspaceIcon"
    This reverts commit 237610d59de6ab3c6f287340dc9b6d96111be7f8.
    
    Reason for revert: b/425692064 no longer reproducing
    
    Fixes: 425692064
    Change-Id: I88df0fd8e5bb69d8423f38f840eb2cc464b616dd

--- tests/src_e2e/com/android/launcher3/dragging/TaplUninstallRemoveTest.java
+++ tests/src_e2e/com/android/launcher3/dragging/TaplUninstallRemoveTest.java
@@ -32,7 +32,6 @@ import com.android.launcher3.tapl.HomeAllApps;
 import com.android.launcher3.tapl.HomeAppIcon;
 import com.android.launcher3.tapl.Workspace;
 import com.android.launcher3.util.TestUtil;
-import com.android.launcher3.util.rule.ScreenRecordRule;
 import com.android.launcher3.util.ui.AbstractLauncherUiTest;
 import com.android.launcher3.util.ui.PortraitLandscapeRunner.PortraitLandscape;
 
@@ -126,7 +125,6 @@ public class TaplUninstallRemoveTest extends AbstractLauncherUiTest<Launcher, Vi
      */
     @Test
     @PlatinumTest(focusArea = "launcher")
-    @ScreenRecordRule.ScreenRecord // TODO(b/425692064): remove when closing bug
     public void uninstallWorkspaceIcon() throws IOException {
         Point[] gridPositions = TestUtil.getCornersAndCenterPositions(mLauncher);
         StringBuilder sb = new StringBuilder();
