d061fec5468f820af5679f67cd330a93be1ccb5f
Fengjiang Li <fengjial@google.com>
2025-09-30 21:29:57 +0000

    [Taskbar][15/n] Ensure BubbleBarInputConsumer invoke bubble bar API on taskbar ui thread
    This CL also add a few bubble bar state to TaskbarUiState
    
    Test: presubmit
    Bug: 404636836
    Flag: com.android.launcher3.enable_taskbar_ui_thread
    Flag: com.android.launcher3.refactor_taskbar_ui_state
    Change-Id: Id0d9845c167f25710f6ed827fe579a998e549f6e

--- quickstep/src/com/android/launcher3/taskbar/bubbles/BubbleBarViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/bubbles/BubbleBarViewController.java
@@ -176,6 +176,7 @@ public class BubbleBarViewController {
         mTaskbarUiState = taskbarUiState;
         mBarView = barView;
         if (refactorTaskbarUiState()) {
+            mBarView.setTaskbarUiState(taskbarUiState);
             mBarView.setOnHierarchyChangeListener(new ViewGroup.OnHierarchyChangeListener() {
                 @Override
                 public void onChildViewAdded(View view, View view1) {
