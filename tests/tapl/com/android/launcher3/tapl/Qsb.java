a3e6bea0a9b255eb92c5572547c1fe9db11c15dd
Jagrut Desai <jagrutdesai@google.com>
2025-10-01 11:21:10 -0700

    Remove Unsed methods form Tapl Qsb
    Test: Presubmit
    Bug: 402832832
    Flag: EXEMPT TEST_ONLY
    Change-Id: I04740c45bcfa979502b372621beb7ecbc752a25e

--- tests/tapl/com/android/launcher3/tapl/Qsb.java
+++ tests/tapl/com/android/launcher3/tapl/Qsb.java
@@ -29,11 +29,6 @@ import java.util.regex.Pattern;
  */
 public abstract class Qsb implements SearchInputSource {
 
-    private static final String ASSISTANT_APP_PACKAGE = "com.google.android.googlequicksearchbox";
-    private static final String ASSISTANT_ICON_RES_ID = "mic_icon";
-    private static final String LENS_ICON_RES_ID = "lens_icon";
-    private static final Pattern LENS_APP_RES_PATTERN = Pattern.compile(
-            ASSISTANT_APP_PACKAGE + ":id/lens.*");
     protected final LauncherInstrumentation mLauncher;
     private final UiObject2 mContainer;
     private final String mQsbResName;
@@ -48,67 +43,6 @@ public abstract class Qsb implements SearchInputSource {
     // Waits for the quick search box.
     private UiObject2 waitForQsbObject() {
         return mLauncher.waitForObjectInContainer(mContainer, mQsbResName);
-    }
-
-    /**
-     * Launch assistant app by tapping mic icon on qsb.
-     */
-
-    @NonNull
-    public LaunchedAppState launchAssistant() {
-        try (LauncherInstrumentation.Closable c = mLauncher.addContextLayer(
-                "want to click assistant mic icon button");
-             LauncherInstrumentation.Closable e = mLauncher.eventsCheck()) {
-            UiObject2 assistantIcon = mLauncher.waitForLauncherObject(ASSISTANT_ICON_RES_ID);
-
-            LauncherInstrumentation.log("Qsb.launchAssistant before click "
-                    + assistantIcon.getVisibleCenter() + " in "
-                    + mLauncher.getVisibleBounds(assistantIcon));
-
-            mLauncher.clickLauncherObject(assistantIcon);
-
-            try (LauncherInstrumentation.Closable c2 = mLauncher.addContextLayer("clicked")) {
-                // assert Assistant App Launched
-                BySelector selector = By.pkg(ASSISTANT_APP_PACKAGE);
-                mLauncher.assertTrue(
-                        "assistant app didn't start: (" + selector + ")",
-                        mLauncher.getDevice().wait(Until.hasObject(selector),
-                                LauncherInstrumentation.WAIT_TIME_MS)
-                );
-                return new LaunchedAppState(mLauncher);
-            }
-        }
-    }
-
-    /**
-     * Launches lens app by tapping lens icon on qsb.
-     */
-    @NonNull
-    public LaunchedAppState launchLens() {
-        try (LauncherInstrumentation.Closable c = mLauncher.addContextLayer(
-                "want to click lens icon button");
-             LauncherInstrumentation.Closable e = mLauncher.eventsCheck()) {
-            UiObject2 lensIcon = mLauncher.waitForLauncherObject(LENS_ICON_RES_ID);
-
-            LauncherInstrumentation.log("Qsb.launchLens before click "
-                    + lensIcon.getVisibleCenter() + " in "
-                    + mLauncher.getVisibleBounds(lensIcon));
-
-            mLauncher.clickLauncherObject(lensIcon);
-
-            try (LauncherInstrumentation.Closable c2 = mLauncher.addContextLayer("clicked")) {
-                // Package name is not enough to check if the app is launched, because many
-                // elements are having googlequicksearchbox as package name. So it checks if the
-                // corresponding app resource is displayed
-                BySelector selector = By.res(LENS_APP_RES_PATTERN);
-                mLauncher.assertTrue(
-                        "Lens app didn't start: (" + selector + ")",
-                        mLauncher.getDevice().wait(Until.hasObject(selector),
-                                LauncherInstrumentation.WAIT_TIME_MS)
-                );
-                return new LaunchedAppState(mLauncher);
-            }
-        }
     }
 
     /**
