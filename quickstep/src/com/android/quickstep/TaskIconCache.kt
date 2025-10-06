ae38424d25001f9e5ad8f3bfac2aff8a04747636
Fengjiang Li <fengjial@google.com>
2025-10-06 23:07:26 +0000

    Clean up TaskIconCache's listener on display changes
    Follow up a dicussion in ag/36145025
    
    Bug: 404636836
    Test: presubmit
    Flag: NONE - released code
    Change-Id: Ic4652cfd1b46001a7e1b32f4706124a469d975a8

--- quickstep/src/com/android/quickstep/TaskIconCache.kt
+++ quickstep/src/com/android/quickstep/TaskIconCache.kt
@@ -33,6 +33,7 @@ import com.android.launcher3.icons.IconProvider
 import com.android.launcher3.icons.LauncherIcons
 import com.android.launcher3.pm.UserCache
 import com.android.launcher3.util.CancellableTask
+import com.android.launcher3.util.DaggerSingletonTracker
 import com.android.launcher3.util.DisplayController
 import com.android.launcher3.util.Executors
 import com.android.launcher3.util.Executors.MAIN_EXECUTOR
@@ -56,6 +57,7 @@ class TaskIconCache(
     private val iconProvider: IconProvider,
     displayController: DisplayController,
     val dispatcherProvider: DispatcherProvider,
+    daggerSingletonTracker: DaggerSingletonTracker,
 ) : TaskIconDataSource {
     private val recentsIconCacheSize = context.resources.getInteger(R.integer.recentsIconCacheSize)
     private var iconCache: TaskKeyLruCache<TaskCacheEntry>? = null
@@ -83,7 +85,9 @@ class TaskIconCache(
         // TODO (b/397205964): this will need to be updated when we support caches for different
         //  displays.
         displayController.listenable?.let {
-            it.changes.forEach(MAIN_EXECUTOR) { flags -> onDisplayInfoChanged(flags) }
+            daggerSingletonTracker.addCloseable(
+                it.changes.forEach(MAIN_EXECUTOR) { flags -> onDisplayInfoChanged(flags) }
+            )
         }
     }
 
