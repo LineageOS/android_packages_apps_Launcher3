ed25ab6b2aa2c313605aff0c4301c28c8e277f85
Fengjiang Li <fengjial@google.com>
2025-10-08 21:51:48 +0000

    [Taskbar] Fix typo that causes recursive call
    Fix: 450354761
    Flag: com.android.launcher3.refactor_taskbar_ui_state
    Test: presubmit
    Change-Id: I37911b0f493c33b2d9a0cefe7e7e672ff32782b8

--- quickstep/src/com/android/quickstep/inputconsumers/TaskbarUnstashInputConsumer.java
+++ quickstep/src/com/android/quickstep/inputconsumers/TaskbarUnstashInputConsumer.java
@@ -467,7 +467,7 @@ public class TaskbarUnstashInputConsumer extends DelegateInputConsumer {
             }
             return ret;
         } else {
-            return getTaskbarSlowVelocityYThreshold();
+            return legacyGetTaskbarSlowVelocityYThreshold();
         }
     }
 
