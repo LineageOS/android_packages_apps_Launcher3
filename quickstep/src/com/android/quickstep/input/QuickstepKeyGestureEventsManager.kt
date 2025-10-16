ea3332f4a8dde778d551fab9db656df1d0e8cc7d
Fengjiang Li <fengjial@google.com>
2025-10-16 21:53:05 +0000

    Fix flaky QuickstepKeyGestureEventsHandlerTest
    Similar to ag/36327249, since test may not be running on main thread,
    and since QuickstepKeyGestureEventsManager just update a flag in setting
    cache's listener, we can use IMMEDIATE_EXECUTOR rather than MAIN_EXECUTOR
    
    Bug: 450563461
    Test: com.android.quickstep.input.QuickstepKeyGestureEventsHandlerTest
    Flag: NONE - released code
    Change-Id: I09203b08e4859b7ba082deef50c2d0a0f567034c

--- quickstep/src/com/android/quickstep/input/QuickstepKeyGestureEventsManager.kt
+++ quickstep/src/com/android/quickstep/input/QuickstepKeyGestureEventsManager.kt
@@ -37,7 +37,7 @@ import androidx.annotation.VisibleForTesting
 import com.android.launcher3.dagger.ApplicationContext
 import com.android.launcher3.dagger.LauncherAppSingleton
 import com.android.launcher3.util.DaggerSingletonObject
-import com.android.launcher3.util.Executors.MAIN_EXECUTOR
+import com.android.launcher3.util.Executors.IMMEDIATE_EXECUTOR
 import com.android.launcher3.util.SafeCloseable
 import com.android.launcher3.util.SettingsCache
 import com.android.quickstep.OverviewCommandHelper
@@ -60,11 +60,12 @@ constructor(@ApplicationContext private val context: Context, settingsCache: Set
     private var allAppsPendingIntent: PendingIntent? = null
     private var overviewGestureHandler: OverviewGestureHandler? = null
     private var overviewCommandHelper: OverviewCommandHelper? = null
-    private var isUserSetupCompleted: Boolean = false
+    @Volatile private var isUserSetupCompleted: Boolean = false
 
     init {
         onUserSetupCompleteSafeCloseable =
-            settingsCache.getListenableRef(USER_SETUP_COMPLETE_URI).forEach(MAIN_EXECUTOR) { v ->
+            settingsCache.getListenableRef(USER_SETUP_COMPLETE_URI).forEach(IMMEDIATE_EXECUTOR) { v
+                ->
                 isUserSetupCompleted = v
             }
     }
