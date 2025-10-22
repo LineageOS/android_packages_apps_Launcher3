91e92ef4ff10d0461d3ac9644668722e9e0a2727
Matthew Mourgos <mmourgos@google.com>
2025-10-22 11:27:01 -0700

    Add WorkspaceSelectionManager for workspace item selection
    Introduces the initial WorkspaceSelectionManager to handle selection of workspace items (icons and folders).
    
    Includes:
    - WorkspaceSelectionManager interface with Stub and Impl.
    - Workspace integration to supply selectable items.
    - Dagger provider, flag guarded by 'enable_cursor_driven_workflows'.
    - Unit tests for selection logic.
    
    Bug: 430287769
    Flag: com.android.launcher3.enable_cursor_driven_workflows
    Test: WorkspaceSelectionManagerImplTest
    Change-Id: I9f385915e87edf161bd5a136f3413eaf895ddc62

---
+++ src/com/android/launcher3/WorkspaceSelectionManager.kt
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3
+
+import android.graphics.Rect
+import android.view.View
+import com.android.launcher3.WorkspaceSelectionManager.ItemSelectionType
+import com.android.launcher3.apppairs.AppPairIcon
+import com.android.launcher3.dagger.ActivityContextSingleton
+import com.android.launcher3.folder.Folder
+import com.android.launcher3.folder.FolderIcon
+import com.android.launcher3.model.data.ItemInfo
+import com.android.launcher3.util.LauncherBindableItemsContainer.ItemOperator
+import com.android.launcher3.views.ActivityContext
+import javax.inject.Inject
+
+/**
+ * Manages setting the selection state of items within the Workspace.
+ *
+ * Responsible for:
+ * - Handling different selection gestures:
+ *     - Single selection (select one item, deselecting others).
+ *     - Toggle selection (adding/removing individual items from the selection).
+ *     - Range selection (selecting a group of items between an anchor and a new item).
+ *     - Box selection (selecting all items within a drawn rectangle).
+ * - Applying the selected state to the views.
+ */
+sealed interface WorkspaceSelectionManager {
+
+    enum class ItemSelectionType {
+        // Selects a single item, deselecting any other currently selected items.
+        SELECT_SINGLE,
+        // Toggles the selection state of a single item, leaving other selections unchanged.
+        TOGGLE_SINGLE,
+        // Selects a range of items between a previously selected anchor item and the current item.
+        RANGE,
+    }
+
+    /**
+     * Handles a selection event on a single item.
+     *
+     * @param view The view that was interacted with.
+     * @param itemSelectionType The type of selection to perform.
+     */
+    fun updateItemSelection(view: View, itemSelectionType: ItemSelectionType)
+
+    /**
+     * Handles a box selection event.
+     *
+     * @param boxBounds The rectangle defining the selection area in window coordinates.
+     */
+    fun onBoxSelection(boxBounds: Rect)
+}
+
+@ActivityContextSingleton
+class WorkspaceSelectionManagerStub @Inject constructor() : WorkspaceSelectionManager {
+    override fun updateItemSelection(view: View, itemSelectionType: ItemSelectionType) = Unit
+
+    override fun onBoxSelection(boxBounds: Rect) = Unit
+}
+
+@ActivityContextSingleton
+class WorkspaceSelectionManagerImpl @Inject constructor(val activityContext: ActivityContext) :
+    WorkspaceSelectionManager {
+
+    private fun getSelectableViews(): List<View> {
+        val items = ArrayList<View>()
+        val op = ItemOperator { info: ItemInfo?, view: View ->
+            items.add(view)
+            false // Return false to continue iterating through all the items.
+        }
+
+        val openFolder = Folder.getOpen(activityContext)
+        if (openFolder != null) {
+            openFolder.mapOverVisibleItems(op)
+        } else {
+            activityContext.content.mapOverVisibleItems(op)
+        }
+        return items
+    }
+
+    /**
+     * The view that serves as the fixed point for range selections. When a range selection (e.g.,
+     * shift-click) occurs, the selection will include all items between this [anchorView] and the
+     * newly selected item, based on their row-major order in the grid.
+     */
+    private var anchorView: View? = null
+
+    override fun updateItemSelection(view: View, itemSelectionType: ItemSelectionType) {
+        when (itemSelectionType) {
+            ItemSelectionType.SELECT_SINGLE -> {
+                performSelection(listOf(view), SelectionAction.NEW_SELECTION)
+            }
+            ItemSelectionType.TOGGLE_SINGLE -> {
+                performSelection(listOf(view), SelectionAction.TOGGLE_SELECTION)
+            }
+            ItemSelectionType.RANGE -> {
+                // TODO: Calculate the list of views to select between [view] and [anchorView].
+                performSelection(emptyList(), SelectionAction.APPEND_SELECTION)
+            }
+        }
+        anchorView = view
+    }
+
+    override fun onBoxSelection(boxBounds: Rect) {
+        /**
+         * TODO: Implement box selection. Given a rect of [boxBounds], perform
+         *   [SelectionAction.NEW_SELECTION] or [SelectionAction.APPEND_SELECTION] on items
+         *   contained within the box.
+         */
+    }
+
+    private enum class SelectionAction {
+        // Creates a new selection, deselecting any previously selected items.
+        NEW_SELECTION,
+        // Adds items to the current selection.
+        APPEND_SELECTION,
+        // Toggles the selection state of the target items.
+        TOGGLE_SELECTION,
+    }
+
+    /**
+     * Performs the selection action on the given list of views.
+     *
+     * @param targetViews The list of views to perform the selection action on. This is a subset of
+     *   the views provided by [getSelectableViews].
+     * @param action The selection action to perform.
+     */
+    private fun performSelection(targetViews: List<View>, action: SelectionAction) {
+        when (action) {
+            SelectionAction.NEW_SELECTION -> {
+                // Reset all selectable views to no longer be selected.
+                for (view in getSelectableViews()) {
+                    setViewSelected(view, false)
+                }
+                // Set only the targetViews as selected for the new selection.
+                for (view in targetViews) {
+                    setViewSelected(view, true)
+                }
+            }
+            SelectionAction.APPEND_SELECTION -> {
+                for (view in targetViews) {
+                    setViewSelected(view, true)
+                }
+            }
+            SelectionAction.TOGGLE_SELECTION -> {
+                for (view in targetViews) {
+                    toggleViewSelected(view)
+                }
+            }
+        }
+    }
+
+    private fun toggleViewSelected(view: View) {
+        if (isViewSelectable(view)) {
+            view.isSelected = !view.isSelected
+            view.invalidate()
+        }
+    }
+
+    private fun setViewSelected(view: View, isSelected: Boolean) {
+        if (isViewSelectable(view)) {
+            view.isSelected = isSelected
+            view.invalidate()
+        }
+    }
+
+    private fun isViewSelectable(view: View): Boolean {
+        return (view is BubbleTextView || view is FolderIcon || view is AppPairIcon)
+    }
+}
---
+++ tests/multivalentTests/src/com/android/launcher3/WorkspaceSelectionManagerImplTest.kt
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3
+
+import android.content.Context
+import android.view.View
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.android.launcher3.WorkspaceSelectionManager.ItemSelectionType
+import com.android.launcher3.folder.FolderIcon
+import com.android.launcher3.model.data.ItemInfo
+import com.android.launcher3.util.LauncherBindableItemsContainer
+import com.android.launcher3.util.LauncherBindableItemsContainer.ItemOperator
+import com.android.launcher3.views.ActivityContext
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.anyBoolean
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.whenever
+
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class WorkspaceSelectionManagerImplTest {
+
+    private lateinit var selectionManager: WorkspaceSelectionManagerImpl
+    private lateinit var view1: View
+    private lateinit var view2: View
+    private lateinit var view3: View
+    private lateinit var allViews: List<View>
+    private val context: Context = mock(Context::class.java)
+
+    private val mockActivityContext = mock(ActivityContext::class.java)
+    private val mockItemsContainer = mock(LauncherBindableItemsContainer::class.java)
+
+    // Helper function to create mock views
+    private fun createMockBubbleTextView(): BubbleTextView {
+        val mockView = mock(BubbleTextView::class.java)
+        whenever(mockView.context).thenReturn(context)
+        var selected = false
+        whenever(mockView.isSelected).thenAnswer { selected }
+        whenever(mockView.setSelected(anyBoolean())).thenAnswer { invocation ->
+            selected = invocation.arguments[0] as Boolean
+            null
+        }
+        whenever(mockView.tag).thenReturn(ItemInfo())
+        return mockView
+    }
+
+    private fun createMockFolderIcon(): FolderIcon {
+        val mockView = mock(FolderIcon::class.java)
+        whenever(mockView.context).thenReturn(context)
+        var selected = false
+        whenever(mockView.isSelected).thenAnswer { selected }
+        whenever(mockView.setSelected(anyBoolean())).thenAnswer { invocation ->
+            selected = invocation.arguments[0] as Boolean
+            null
+        }
+        whenever(mockView.tag).thenReturn(ItemInfo())
+        return mockView
+    }
+
+    @Before
+    fun setUp() {
+        whenever(mockActivityContext.content).thenReturn(mockItemsContainer)
+        selectionManager = WorkspaceSelectionManagerImpl(mockActivityContext)
+
+        view1 = createMockBubbleTextView()
+        view2 = createMockFolderIcon()
+        view3 = createMockBubbleTextView()
+        allViews = listOf(view1, view2, view3)
+
+        // Stub the mapOverVisibleItems call
+        doAnswer { invocation ->
+                val op = invocation.getArgument<ItemOperator>(0)
+                for (view in allViews) {
+                    op.evaluate(view.tag as ItemInfo, view)
+                }
+                null
+            }
+            .whenever(mockItemsContainer)
+            .mapOverVisibleItems(any())
+    }
+
+    @Test
+    fun testSelectSingleNode_selectsOneDeselectsOthers() {
+        // Initial state: view2 is selected
+        view2.isSelected = true
+        selectionManager.updateItemSelection(view1, ItemSelectionType.SELECT_SINGLE)
+
+        assertTrue(view1.isSelected)
+        assertFalse(view2.isSelected)
+        assertFalse(view3.isSelected)
+    }
+
+    @Test
+    fun testToggleSingle_togglesSelectionState() {
+        // Initial state: view1 not selected
+        assertFalse(view1.isSelected)
+        selectionManager.updateItemSelection(view1, ItemSelectionType.TOGGLE_SINGLE)
+        assertTrue(view1.isSelected)
+
+        // Toggle again
+        selectionManager.updateItemSelection(view1, ItemSelectionType.TOGGLE_SINGLE)
+        assertFalse(view1.isSelected)
+    }
+
+    @Test
+    fun testNewSelection_replacesExistingSelection() {
+        // Initial state: view1 and view2 are selected
+        view1.isSelected = true
+        view2.isSelected = true
+        selectionManager.updateItemSelection(view3, ItemSelectionType.SELECT_SINGLE)
+
+        assertFalse(view1.isSelected)
+        assertFalse(view2.isSelected)
+        assertTrue(view3.isSelected)
+    }
+
+    @Test
+    fun testToggleMultiple_appendsToSelection() {
+        // Initial state: view1 is selected
+        view1.isSelected = true
+        selectionManager.updateItemSelection(view2, ItemSelectionType.TOGGLE_SINGLE)
+        selectionManager.updateItemSelection(view3, ItemSelectionType.TOGGLE_SINGLE)
+
+        assertTrue(view1.isSelected)
+        assertTrue(view2.isSelected)
+        assertTrue(view3.isSelected)
+    }
+}
