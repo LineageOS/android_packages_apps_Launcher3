04571a853d4b14a93bf6f894cc869a26eb508760
Jagrut Desai <jagrutdesai@google.com>
2025-11-20 11:09:46 -0800

    Prevent Taskbar pinning animation from running in 3button navigation mode.
    Test: Presubmit, Manual
    Bug: 454507249
    Flag: EXEMPT bugfix
    Change-Id: Iaac2afb699690535a56b285c1b825adda93f77a5

--- quickstep/src/com/android/launcher3/taskbar/TaskbarViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarViewController.java
@@ -554,6 +554,7 @@ public class TaskbarViewController implements TaskbarControllers.LoggableTaskbar
      * Applies scale properties for the taskbar icons
      */
     private void updateTaskbarIconsScale() {
+        if (mActivity.isThreeButtonNav()) return;
         float scale = mTaskbarIconScaleForPinning.value;
         View[] iconViews = mTaskbarView.getIconViews();
 
@@ -587,6 +588,7 @@ public class TaskbarViewController implements TaskbarControllers.LoggableTaskbar
     }
 
     void updateTaskbarIconTranslationXForPinning(boolean updateShiftXForBubbleBar) {
+        if (mActivity.isThreeButtonNav()) return;
         View[] iconViews = mTaskbarView.getIconViews();
         float scale = mTaskbarIconTranslationXForPinning.value;
         float transientTaskbarAllAppsOffset = mActivity.getResources().getDimension(
@@ -738,7 +740,7 @@ public class TaskbarViewController implements TaskbarControllers.LoggableTaskbar
      * Computes translation y for taskbar pinning.
      */
     private float getTaskbarIconTranslationYForPinningValue() {
-        if (mControllers.getSharedState() == null) return 0f;
+        if (mControllers.getSharedState() == null || mActivity.isThreeButtonNav()) return 0f;
 
         float scale = mTaskbarIconTranslationYForPinning.value;
         float taskbarIconTranslationYForPinningValue;
