6a3da5dbce75437d8ceac05033e5543e25ff492c
Shuming Hao <shuminghao@google.com>
2025-12-03 20:29:14 +0000

    [CD] Allow keyboard quick switch (Alt+Tab) for splitscreen
    Keyboard Quick Switch (Alt+Tab) currently fails to launch split-screen pairs on Connected Displays (CD). This is due to a missing implementation in the CD path. The call chain, which routes through `TaskbarUIController.launchSplitTasks`, was only overridden in `LauncherTaskbarUIController` (used on the default display only). This CL overrides `launchSplitTasks` in `FallbackTaskbarUIController` to provide the required logic on CD, enabling successful launch of split-screen pairs via Alt+Tab on CD.
    
    Bug: 457927469
    Test: manual
    Flag: EXEMPT BUGFIX
    Change-Id: I8bbb1b0657aaa568cf65a84fc66c42f8c91bf780

--- quickstep/src/com/android/launcher3/taskbar/FallbackTaskbarUIController.java
+++ quickstep/src/com/android/launcher3/taskbar/FallbackTaskbarUIController.java
@@ -21,15 +21,19 @@ import static com.android.launcher3.taskbar.TaskbarStashController.FLAG_IN_APP;
 import static com.android.launcher3.taskbar.TaskbarStashController.FLAG_IN_STASHED_LAUNCHER_STATE;
 import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
 import static com.android.launcher3.util.Executors.TASKBAR_UI_THREAD;
+import static com.android.wm.shell.shared.split.SplitScreenConstants.SNAP_TO_2_50_50;
 
 import android.animation.Animator;
+import android.window.RemoteTransition;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.android.launcher3.popup.SystemShortcut;
 import com.android.launcher3.statemanager.StateManager;
 import com.android.launcher3.statemanager.StatefulContainer;
 import com.android.launcher3.util.ImmediateAnimator;
+import com.android.launcher3.util.SplitConfigurationOptions;
 import com.android.launcher3.util.TaskbarAsyncAnimator;
 import com.android.launcher3.util.ThreadedAnimator;
 import com.android.quickstep.FallbackActivityInterface;
@@ -37,6 +41,8 @@ import com.android.quickstep.GestureState;
 import com.android.quickstep.RecentsAnimationCallbacks;
 import com.android.quickstep.TopTaskTracker;
 import com.android.quickstep.fallback.RecentsState;
+import com.android.quickstep.split.SplitSelectStateController;
+import com.android.quickstep.util.SplitTask;
 import com.android.quickstep.views.RecentsViewContainer;
 
 import java.io.PrintWriter;
@@ -149,6 +155,27 @@ public class FallbackTaskbarUIController
         if (anim != null) {
             anim.start();
         }
+    }
+
+    @Override
+    public void launchSplitTasks(
+            @NonNull SplitTask splitTask, @Nullable RemoteTransition remoteTransition) {
+        SplitSelectStateController splitSelectStateController =
+                mRecentsContainer.getSplitSelectStateController();
+        if (splitSelectStateController == null) {
+            return;
+        }
+
+        splitSelectStateController.launchExistingSplitPair(null /* launchingTaskView */,
+                splitTask.getTopLeftTask().key.id,
+                splitTask.getBottomRightTask().key.id,
+                SplitConfigurationOptions.STAGE_POSITION_TOP_OR_LEFT,
+                /* callback= */ success -> splitSelectStateController.resetState(),
+                /* freezeTaskList= */ false,
+                splitTask.getSplitBounds() == null
+                        ? SNAP_TO_2_50_50
+                        : splitTask.getSplitBounds().snapPosition,
+                remoteTransition);
     }
 
     @Override
