a3b6eaedb6962103e7dbb33c987841edaf41b434
Pierre Barbier de Reuille <pbdr@google.com>
2025-12-03 15:37:37 +0000

    Choose the right windowing mode for all taskbar launches.
    Instead of always launching in fullscreen for shortcuts or promises,
    select the windowing mode depending on the windowing context.
    
    Fix: 459864133
    Flag: EXEMPT BUGFIX
    Test: manual -- open an app from a shortcut
    Change-Id: Ibacbb6a4fe08baa0f00b5ba2466119fe30a663c0

--- quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -15,6 +15,7 @@
  */
 package com.android.launcher3.taskbar;
 
+import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
 import static android.app.WindowConfiguration.WINDOWING_MODE_FULLSCREEN;
 import static android.os.Trace.TRACE_TAG_APP;
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
@@ -1139,18 +1140,25 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
         return makeDefaultActivityOptions(SPLASH_SCREEN_STYLE_UNDEFINED);
     }
 
+    private ActivityOptionsWrapper getSingleActivityLaunchOptions(@Nullable ItemInfo item) {
+        return getSingleActivityLaunchOptions(item,
+                shouldLaunchInDesktop(getDisplayId(), item) ? WINDOWING_MODE_FREEFORM
+                        : WINDOWING_MODE_FULLSCREEN);
+    }
+
+    private ActivityOptionsWrapper getFullscreenActivityLaunchOptions(@Nullable ItemInfo item) {
+        return getSingleActivityLaunchOptions(item, WINDOWING_MODE_FULLSCREEN);
+    }
+
     /**
      * Returns activity options for launching a single activity from the taskbar on the display
      * associated with the taskbar.
      */
-    private ActivityOptionsWrapper getSingleActivityLaunchOptions(@Nullable ItemInfo item) {
+    private ActivityOptionsWrapper getSingleActivityLaunchOptions(@Nullable ItemInfo item,
+            int windowingMode) {
         final ActivityOptionsWrapper opts = getActivityLaunchOptions(null, item);
         opts.options.setLaunchDisplayId(getDisplayId());
-        // Launch single non-desktop activities in fullscreen to match launches from the
-        // hotseat. This needs to be explicitly set to ensure that tasks in other windowing
-        // modes are moved to fullscreen as well (otherwise they are shown in their existing
-        // mode)
-        opts.options.setLaunchWindowingMode(WINDOWING_MODE_FULLSCREEN);
+        opts.options.setLaunchWindowingMode(windowingMode);
         return opts;
     }
 
@@ -2094,7 +2102,7 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
             if (shouldLaunchInDesktop(displayId, info)) {
                 launchDesktopApp(intent, info, displayId);
             } else {
-                startActivity(intent, getSingleActivityLaunchOptions(info).toBundle());
+                startActivity(intent, getFullscreenActivityLaunchOptions(info).toBundle());
             }
         } catch (NullPointerException | ActivityNotFoundException | SecurityException e) {
             Toast.makeText(this, R.string.activity_not_found, Toast.LENGTH_SHORT)
