e5afc07a0335e52b06ad1fa9b09e9b9326deef8f
Andy Wickham <awickham@google.com>
2025-12-05 19:05:32 -0800

    Prevent Taskbar touches from slipping to adjacent apps.
    This is particularly likely to happen if Taskbar changes size during
    a touch, e.g. invoking Contextual Search from the All Apps button in
    Overview, which causes Taskbar to stash.
    
    Touch slipping is still allowed in 3 button nav since most likely the
    user intended to scroll in the underlying app in that case.
    
    Fix: 459347535
    Test: Manual by invoking Contextual Search in overview
    Flag: EXEMPT BUGFIX
    
    Change-Id: I1ba3c4e7f3b3eca295506e03bf77a2ac6fcbd8f4

--- quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -814,8 +814,11 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
      * @param title The window title to pass to the created WindowManager.LayoutParams.
      */
     public WindowManager.LayoutParams createDefaultWindowLayoutParams(int type, String title) {
-        int windowFlags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                | WindowManager.LayoutParams.FLAG_SLIPPERY;
+        int windowFlags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        if (!isTransientTaskbar()) {
+            // Allow apps to receive swipe events from non-transient taskbar (e.g. 3 button nav).
+            windowFlags |= WindowManager.LayoutParams.FLAG_SLIPPERY;
+        }
         boolean watchOutside = isTransientTaskbar() || isThreeButtonNav();
         if (watchOutside && !isRunningInTestHarness()) {
             windowFlags |= WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
