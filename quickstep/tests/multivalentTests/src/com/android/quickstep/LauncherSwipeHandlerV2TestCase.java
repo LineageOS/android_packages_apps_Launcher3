b928f66535adbdfc84daf33001101bc2d406fa05
Anushree Ganjam <ganjam@google.com>
2025-12-09 14:52:25 -0800

    Add bug number for @Ignore in LauncherSwipeHandlerV2TestCase.java
    Bug: 467431341
    Test: Manual
    Flag: TEST_ONLY
    Change-Id: Ia1a4572c1ee02b8ce04e4cc2cb06aa254ed80558

--- quickstep/tests/multivalentTests/src/com/android/quickstep/LauncherSwipeHandlerV2TestCase.java
+++ quickstep/tests/multivalentTests/src/com/android/quickstep/LauncherSwipeHandlerV2TestCase.java
@@ -39,7 +39,7 @@ import org.mockito.Mock;
 
 @SmallTest
 @RunWith(LauncherMultivalentJUnit.class)
-@Ignore
+@Ignore("b/467431341")
 public class LauncherSwipeHandlerV2TestCase extends AbsSwipeUpHandlerTestCase<
         LauncherState,
         QuickstepLauncher,
