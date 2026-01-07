a4189f5aaaebbedd0229f8394909f09f06a6b100
Sebastian Franco <fransebas@google.com>
2026-01-07 13:48:31 -0600

    Remove temporary Launcher setting used for manual testing.
    Test: compiling, this was used before the WallpaperPicker integration
    Bug: 456634299
    Flag: com.android.systemui.shared.workspace_items_label_hidden
    Change-Id: Ie349f0cce7bb3119d4a36650c32c0210c6afd711

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -71,8 +71,6 @@ public class SettingsActivity extends FragmentActivity
 
     public static final String FIXED_LANDSCAPE_MODE = "pref_fixed_landscape_mode";
 
-    public static final String WORKSPACE_ITEMS_LABEL_HIDDEN = "pref_workspace_items_label_hidden";
-
     private static final String NOTIFICATION_DOTS_PREFERENCE_KEY = "pref_icon_badging";
 
     public static final String EXTRA_FRAGMENT_ARGS = ":settings:fragment_args";
@@ -330,8 +328,6 @@ public class SettingsActivity extends FragmentActivity
                             }
                     );
                     return !info.isLargeScreen(info.realBounds);
-                case WORKSPACE_ITEMS_LABEL_HIDDEN:
-                    return com.android.systemui.shared.Flags.workspaceItemsLabelHidden();
             }
             return true;
         }
