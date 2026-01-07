6a704ed955207cd6870e09e3cd596b1c0f0f00eb
David Black <dmblack@google.com>
2026-01-07 21:47:16 +0000

    Improve default drag image for system drags.
    The default drag image for system drags is only shown just after drop
    but before the final icon for the created item info can be rendered.
    Prior to this CL, the default drag image was a blue grid background with
    the android avatar foreground. Now, the default drag image is the
    default placeholder icon. This provides a smoother transition between
    the default drag image and the final icon to be rendered.
    
    Bug: 440196506
    Flag: com.android.launcher3.enable_system_drag
    Test: atest SystemDragListenerTest
    Change-Id: I925ac2547b994a59db2c3e2ede340783aa7122b7

--- quickstep/src/com/android/launcher3/dagger/Modules.kt
+++ quickstep/src/com/android/launcher3/dagger/Modules.kt
@@ -28,6 +28,7 @@ import com.android.internal.R
 import com.android.internal.policy.DesktopModeCompatPolicy
 import com.android.launcher3.AbstractFloatingViewHelper
 import com.android.launcher3.Flags.enableSystemDrag
+import com.android.launcher3.InvariantDeviceProfile
 import com.android.launcher3.automation.AutomationRepository
 import com.android.launcher3.backuprestore.LauncherRestoreEventLogger
 import com.android.launcher3.concurrent.annotations.ThreadPool
@@ -40,7 +41,6 @@ import com.android.launcher3.homescreenfiles.HomeScreenFilesMediaStoreProvider
 import com.android.launcher3.homescreenfiles.HomeScreenFilesNoOpProvider
 import com.android.launcher3.homescreenfiles.HomeScreenFilesProvider
 import com.android.launcher3.homescreenfiles.HomeScreenFilesUtils
-import com.android.launcher3.icons.IconCache
 import com.android.launcher3.icons.LauncherIconProvider
 import com.android.launcher3.icons.LauncherIconProviderImpl
 import com.android.launcher3.logging.StatsLogManager.StatsLogManagerFactory
@@ -81,7 +81,6 @@ import com.android.systemui.shared.system.ActivityManagerWrapper
 import com.android.systemui.shared.system.TaskStackChangeListeners
 import com.android.wm.shell.shared.desktopmode.DesktopState
 import dagger.Binds
-import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.ElementsIntoSet
@@ -211,7 +210,7 @@ object SystemDragModule {
     @ActivityContextSingleton
     fun provideSystemDragController(
         context: ActivityContext,
-        iconCache: Lazy<IconCache>,
+        idp: InvariantDeviceProfile,
     ): SystemDragController =
         // TODO(b/456787959): Fix drop targets before enabling for secondary display launcher.
         // TODO(b/456787959): Fix drop targets before enabling for taskbar.
@@ -222,7 +221,7 @@ object SystemDragModule {
         ) {
             SystemDragControllerImpl(
                 context,
-                { ctx, params -> SystemDragListener(ctx, iconCache, ::ImageView, params) },
+                { ctx, params -> SystemDragListener(ctx, idp, ::ImageView, params) },
             )
         } else {
             SystemDragControllerStub()
