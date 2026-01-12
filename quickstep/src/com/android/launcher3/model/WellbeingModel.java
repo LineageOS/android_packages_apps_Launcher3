c1d763543936b25631d091b2eb544ce6f0cea37b
Alex Chau <alexchau@google.com>
2026-01-12 17:50:32 +0800

    Temporarily enable WellbeingModel in test harness
    Bug: 473048003
    Test: WellbeingTests
    Flag: EXEMPT TEST_ONLY
    Change-Id: Id7b20435db98e9fea586ba4ed4663356194e05c0

--- quickstep/src/com/android/launcher3/model/WellbeingModel.java
+++ quickstep/src/com/android/launcher3/model/WellbeingModel.java
@@ -18,6 +18,7 @@ package com.android.launcher3.model;
 
 import static android.content.ContentResolver.SCHEME_CONTENT;
 
+import static com.android.launcher3.Utilities.isRunningInTestHarness;
 import static com.android.launcher3.util.SimpleBroadcastReceiver.packageFilter;
 
 import android.app.RemoteAction;
@@ -70,7 +71,8 @@ import javax.inject.Inject;
 public final class WellbeingModel implements SafeCloseable {
     private static final String TAG = "WellbeingModel";
     private static final int[] RETRY_TIMES_MS = {5000, 15000, 30000};
-    private static final boolean DEBUG = false;
+    // TODO(b/473048003): Turn off DEBUG flag.
+    private static final boolean DEBUG = isRunningInTestHarness();
 
     // Welbeing contract
     private static final String PATH_ACTIONS = "actions";
