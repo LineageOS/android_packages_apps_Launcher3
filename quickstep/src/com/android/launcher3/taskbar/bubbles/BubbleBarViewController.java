c2676d37e679169bc4801b8ce0a6b7ea4e9b4bd0
Ats Jenk <atsjenk@google.com>
2026-01-14 09:11:17 -0800

    Update bubble a11y actions based on expanded state
    Talkback will infer the expanded or collapsed state of the element based
    on a11y actions. If there is an action for expand, it will first say
    "collapsed" followed by content description of the element. If there is
    an action for collapse, it'll add "expanded".
    To fix this, update a11y actions on individual bubbles to use the state
    of the bubble and not bubble bar itself. And have the actions control
    bubble state. If a bubble is not selected and expanded, it'll get an
    a11y action for expand. That will then select the bubble and expand it.
    
    Also make sure that overflow does not have actions to move the bubble
    bar around to match the experience when using touch interactions.
    
    Bug: 475088812
    Flag: EXEMPT BUGFIX
    Test: manual, have couple of bubbles in the bubble bar, focus on
      expanded bubble, check talkback starts with expanded followed by bubble
      content description, focus on a bubble that is not expanded, check
      talkbak starts with collapsed, followed by bubble content description
    
    Change-Id: Ia2cd776c5c5350075682200dbefaadaf558fde6d

--- quickstep/src/com/android/launcher3/taskbar/bubbles/BubbleBarViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/bubbles/BubbleBarViewController.java
@@ -235,9 +235,6 @@ public class BubbleBarViewController {
         mBubbleClickListener = v -> onBubbleClicked((BubbleView) v);
         mBubbleDragController.setupBubbleBarView(mBarView);
         mOverflowBubble = bubbleControllers.bubbleCreator.createOverflow(mBarView);
-        if (!Flags.enableOptionalBubbleOverflow()) {
-            showOverflow(true);
-        }
         if (!mBubbleStashController.isTransientTaskBar()) {
             // TODO(b/380274085) for transient taskbar mode, the click is also handled by the input
             //  consumer. This check can be removed once b/380274085 is fixed.
@@ -319,6 +316,13 @@ public class BubbleBarViewController {
             }
 
             @Override
+            public void expand(BubbleView bubble) {
+                if (bubble.getBubble() != null) {
+                    mBubbleBarController.showAndSelectBubble(bubble.getBubble());
+                }
+            }
+
+            @Override
             public void collapse() {
                 collapseBubbleBar();
             }
@@ -329,6 +333,11 @@ public class BubbleBarViewController {
                 mBubbleBarController.updateBubbleBarLocation(location, source);
             }
         };
+
+        if (!Flags.enableOptionalBubbleOverflow()) {
+            // This should be called after mBubbleViewController is initialized
+            showOverflow(true);
+        }
     }
 
     /**
