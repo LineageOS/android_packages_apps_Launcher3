e5b6ea8c8e892bbe513d44aa003836b7e5d0620c
Federico Baron <fbaron@google.com>
2026-01-19 07:00:01 +0000

    Add support for new popup menu in ThemeIconsTest
    Test: Manual
    Flag: com.android.launcher3.expandable_long_press_menu
    Bug: 444003708
    Change-Id: I01f58a68215c009075eba75afa13a7e428bc6381

--- tests/multivalentTests/src/com/android/launcher3/workspace/ThemeIconsTest.kt
+++ tests/multivalentTests/src/com/android/launcher3/workspace/ThemeIconsTest.kt
@@ -18,6 +18,9 @@ package com.android.launcher3.workspace
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.net.Uri
+import android.platform.test.annotations.DisableFlags
+import android.platform.test.annotations.EnableFlags
+import android.platform.test.flag.junit.SetFlagsRule
 import android.platform.test.rule.LimitDevicesRule
 import android.platform.test.rule.SkipOnDeviceless
 import android.view.View
@@ -26,12 +29,15 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.android.launcher3.AbstractFloatingView
 import com.android.launcher3.BubbleTextView
+import com.android.launcher3.Flags
 import com.android.launcher3.Launcher
 import com.android.launcher3.Utilities
+import com.android.launcher3.accessibility.LauncherAccessibilityDelegate
 import com.android.launcher3.dagger.LauncherComponentProvider.get
 import com.android.launcher3.icons.mono.ThemedIconDelegate
 import com.android.launcher3.model.data.ItemInfo
 import com.android.launcher3.popup.ArrowPopup
+import com.android.launcher3.popup.PopupContainer
 import com.android.launcher3.util.BaseLauncherActivityTest
 import com.android.launcher3.util.Executors
 import com.android.launcher3.util.ModelTestExtensions.setEmptyModelLayout
@@ -53,7 +59,9 @@ class ThemeIconsTest : BaseLauncherActivityTest<Launcher>() {
      * This test can't run in Robolectric because APP_NAME and TEST_APP_NAME can't found when
      * running the test in Robolectric.
      */
-    @get:Rule var mlimitDevicesRule: LimitDevicesRule = LimitDevicesRule()
+    @get:Rule(order = 0) var mlimitDevicesRule: LimitDevicesRule = LimitDevicesRule()
+    @get:Rule(order = 1)
+    val setFlagsRule = SetFlagsRule(SetFlagsRule.DefaultInitValueType.DEVICE_DEFAULT)
 
     @Test
     @SkipOnDeviceless
@@ -77,7 +85,28 @@ class ThemeIconsTest : BaseLauncherActivityTest<Launcher>() {
     @Test
     @SkipOnDeviceless
     @Throws(Exception::class)
+    @EnableFlags(Flags.FLAG_EXPANDABLE_LONG_PRESS_MENU)
     fun testShortcutIconWithoutTheme() {
+        setThemeEnabled(false)
+        targetContext().setEmptyModelLayout()
+        loadLauncherSync()
+
+        scrollToAppIcon(TEST_APP_NAME)
+        val btv =
+            launcherActivity.getFromLauncher { l: Launcher -> findBtv(TEST_APP_NAME, l.appsView) }
+        TestUtil.runOnExecutorSync(Executors.MAIN_EXECUTOR) { btv!!.performLongClick() }
+        addAppShortcut()
+
+        launcherActivity.executeOnLauncher { l: Launcher ->
+            verifyIconTheme(SHORTCUT_NAME, l.workspace, false)
+        }
+    }
+
+    @Test
+    @SkipOnDeviceless
+    @Throws(Exception::class)
+    @DisableFlags(Flags.FLAG_EXPANDABLE_LONG_PRESS_MENU)
+    fun testShortcutIconWithoutTheme_oldPopup() {
         setThemeEnabled(false)
         targetContext().setEmptyModelLayout()
         loadLauncherSync()
@@ -128,8 +157,30 @@ class ThemeIconsTest : BaseLauncherActivityTest<Launcher>() {
     @Test
     @SkipOnDeviceless
     @Throws(Exception::class)
+    @EnableFlags(Flags.FLAG_EXPANDABLE_LONG_PRESS_MENU)
     fun testShortcutIconWithTheme() {
         setThemeEnabled(true)
+        targetContext().setEmptyModelLayout()
+        loadLauncherSync()
+
+        scrollToAppIcon(TEST_APP_NAME)
+        val btv =
+            launcherActivity.getFromLauncher { l: Launcher -> findBtv(TEST_APP_NAME, l.appsView) }
+        TestUtil.runOnExecutorSync(Executors.MAIN_EXECUTOR) { btv!!.performLongClick() }
+        addAppShortcut()
+
+        launcherActivity.executeOnLauncher { l: Launcher ->
+            verifyIconTheme(SHORTCUT_NAME, l.workspace, true)
+        }
+    }
+
+    @Test
+    @SkipOnDeviceless
+    @Throws(Exception::class)
+    @DisableFlags(Flags.FLAG_EXPANDABLE_LONG_PRESS_MENU)
+    fun testShortcutIconWithTheme_oldPopup() {
+        setThemeEnabled(true)
+        targetContext().setEmptyModelLayout()
         loadLauncherSync()
 
         scrollToAppIcon(TEST_APP_NAME)
@@ -152,6 +203,36 @@ class ThemeIconsTest : BaseLauncherActivityTest<Launcher>() {
         launcherTestInteractions.addToWorkspace(menuItem!!)
         launcherActivity.executeOnLauncher { l: Launcher ->
             verifyIconTheme(SHORTCUT_NAME, l.workspace, true)
+        }
+    }
+
+    private fun addAppShortcut() {
+        // Wait for deep shortcuts to load in ViewModel
+        launcherActivity.waitUntil("Shortcut '$SHORTCUT_NAME' was not loaded in ViewModel") {
+            l: Launcher ->
+            val container = PopupContainer.getOpen(l)
+            container?.viewModel?.state?.deepShortcuts?.any {
+                it?.title?.toString() == SHORTCUT_NAME
+            } == true
+        }
+
+        // Add shortcut to workspace programmatically
+        launcherActivity.executeOnLauncher { l: Launcher ->
+            val container = PopupContainer.getOpen(l)
+            val shortcutInfo =
+                container?.viewModel?.state?.deepShortcuts?.filterNotNull()?.first {
+                    it.title.toString() == SHORTCUT_NAME
+                }
+            (l.accessibilityDelegate as LauncherAccessibilityDelegate).addToWorkspace(
+                shortcutInfo!!,
+                false,
+                null,
+            )
+        }
+
+        launcherActivity.waitUntil("Shortcut '$SHORTCUT_NAME' was not added to workspace") {
+            l: Launcher ->
+            findBtv(SHORTCUT_NAME, l.workspace) != null
         }
     }
 
