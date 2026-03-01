c70d3cb5891bd4ba37b399f5d6bb058aeafa46d6
Tracy Zhou <tracyzhou@google.com>
2026-02-28 22:17:18 -0800

    Support blur in Launcher folders
    We use a combination of cross window blur and render effect of the pre open drag layer screenshot so that we can blend the blurred icons with the blurred wallpaper.
    
    Bug: 445467156
    Test: http://recall/-/cJXSFKMnb60bY3b9qyndqO/jzM1UnC8ULP9wDBUV9m8B
    Flag: com.android.launcher3.blur_on_more_surfaces
    Change-Id: I05cde2128015806bfc3a89677c945da615dab1a5

--- quickstep/src/com/android/launcher3/dagger/Modules.kt
+++ quickstep/src/com/android/launcher3/dagger/Modules.kt
@@ -51,6 +51,8 @@ import com.android.launcher3.model.WellbeingModel
 import com.android.launcher3.secondarydisplay.SecondaryDisplayDelegate
 import com.android.launcher3.LauncherModel
 import com.android.launcher3.ModelReloader
+import com.android.launcher3.folder.FolderBlurBackgroundHelper
+import com.android.launcher3.folder.QuickstepFolderBackgroundBlurHelper
 import com.android.launcher3.secondarydisplay.SecondaryDisplayQuickstepDelegateImpl
 import com.android.launcher3.taskbar.customization.TaskbarFeatureEvaluator
 import com.android.launcher3.testing.TestInformationHandler
@@ -109,6 +111,11 @@ abstract class ActivityContextModule {
     abstract fun bindSecondaryDisplayDelegate(
         impl: SecondaryDisplayQuickstepDelegateImpl
     ): SecondaryDisplayDelegate
+
+    @Binds
+    abstract fun bindFolderBackgroundBlurHelper(
+        quickstepFolderBackgroundBlurHelper: QuickstepFolderBackgroundBlurHelper
+    ): FolderBlurBackgroundHelper
 
     companion object {
         @JvmStatic
