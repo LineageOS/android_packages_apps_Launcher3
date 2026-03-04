b778cf74c69d4bb8d74c5eb9449278dfe0cb45a3
Adnan Begovic <abegovic@google.com>
2026-03-04 13:47:56 -0800

    Introduce Workspace AppFunctions module.
    This change adds the foundational structure for a new WorkspaceAppFunctions module within Launcher3's appfunctions. It includes:
    - Android.bp updates to define build targets for the workspace provider API.
    - WorkspaceAppFunctions: A high-level class for managing the workspace.
    - WorkspaceProvider: An interface for setting and getting the current workspace.
    - WorkspaceFlatTypes: Contains all of the 'flat' types necessary to be inferred by an AppFunction agent.
    
    Bug: 457458301
    Flag: com.android.launcher3.kondo_planner
    Test: N/A, interfaces
    Change-Id: I8453299b8c12d699129cee2b067cf43b3313002f

---
+++ modules/appfunctions/src/com/android/launcher3/appfunctions/workspace/WorkspaceAppFunctions.kt
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.android.launcher3.appfunctions.workspace
+
+import androidx.appfunctions.AppFunctionContext
+import androidx.appfunctions.AppFunctionSerializable
+import androidx.appfunctions.service.AppFunction
+
+/**
+ * High level [AppFunctions] interface for managing the workspace. It is paramount that the
+ * functions, types and documentation are kept in sync with the evaluation framework. Do not commit
+ * changes without testing against the framework.
+ *
+ * @property repository The repository for querying and modifying the workspace.
+ */
+class WorkspaceAppFunctions(private val repository: WorkspaceRepository) {
+
+    /// Query functions
+    /// These are exposed [AppFunction]s that can be called by any client
+    /// of the AppFunctions framework.
+
+    /**
+     * Returns the current [WorkspaceSpec] and a [Proof] token for modifications.
+     *
+     * @param appFunctionContext App function context.
+     * @return [WorkspaceResponse] with workspace and proof.
+     */
+    @AppFunction(isDescribedByKDoc = true)
+    suspend fun getCurrentWorkspace(appFunctionContext: AppFunctionContext): WorkspaceResponse {
+        val workspaceSpec = repository.getWorkspace()
+        return WorkspaceResponse(workspaceSpec, Proof.GET_CURRENT_WORKSPACE_PROOF)
+    }
+
+    /// Decorated responses to the AppFunction agents, the kdoc for these is used
+    // as the raw documentation ingested by the agents.
+
+    /**
+     * Response from [getCurrentWorkspace].
+     *
+     * @property workspace Current [WorkspaceSpec].
+     * @property proof Proof token.
+     */
+    @AppFunctionSerializable(isDescribedByKDoc = true)
+    data class WorkspaceResponse(val workspace: WorkspaceSpec, val proof: Proof)
+
+    /**
+     * Verifies function calls are correctly chained.
+     *
+     * @property token Internal token, never shown to the user.
+     */
+    @AppFunctionSerializable(isDescribedByKDoc = true)
+    data class Proof(val token: String) {
+        companion object {
+            val NO_PROOF = Proof("no_proof")
+            val GET_CURRENT_WORKSPACE_PROOF = Proof("a7em")
+            val MOVE_ITEM_PROOF = Proof("b5d7")
+            val ADD_ITEM_PROOF = Proof("d92b")
+            val CREATE_FOLDER_PROOF = Proof("h618")
+            val REMOVE_ITEM_PROOF = Proof("g27c")
+            val GET_INSTALLED_APPS_PROOF = Proof("i08f")
+            val GET_INSTALLED_WIDGETS_PROOF = Proof("j4b3")
+            val CREATE_SCREEN_PROOF = Proof("k8e9")
+            val REMOVE_SCREEN_PROOF = Proof("l1c7")
+            val ALIGN_SCREEN_PROOF = Proof("m591")
+            val ORGANIZE_SCREEN_PROOF = Proof("n76d")
+            val GET_SCREEN_TEMPLATE_PROOF = Proof("p09a")
+        }
+    }
+}
