02cfa1865fda7482fb56df426900c9feb08d9aa6
Adnan Begovic <abegovic@google.com>
2026-03-06 10:23:23 -0800

    Filter out item info types from reporting to the agent.
    Ensures that the we don't communicate things like
    'Smartspace' or the 'Search bar', etc.
    
    Bug: 457458301
    Flag: com.android.launcher3.kondo_planner
    Test: LauncherWorkspaceTypeTranslatorTest
    Change-Id: I42aef0d68be628dae236fc3499d693b08700dea2

---
+++ tests/multivalentTests/src/com/android/launcher3/appfunctions/workspace/WorkspaceAppFunctionsTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.appfunctions.workspace
+
+import android.content.Context
+import androidx.appfunctions.AppFunctionContext
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.launcher3.appfunctions.workspace.HotseatSpec
+import com.android.launcher3.appfunctions.workspace.WorkspaceAppFunctions.Proof
+import com.android.launcher3.appfunctions.workspace.WorkspaceRepository
+import com.android.launcher3.appfunctions.workspace.WorkspaceSpec
+import com.android.launcher3.appfunctions.workspace.WorkspaceTransaction
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WorkspaceAppFunctionsTest {
+    private val fakeWorkspaceRepository = FakeWorkspaceRepository()
+    private val workspaceAppFunctions =
+        WorkspaceAppFunctions(fakeWorkspaceRepository)
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    @Test
+    fun getCurrentWorkspace_returnsWorkspaceSpec(): Unit = runBlocking {
+        val workspaceResponse =
+            workspaceAppFunctions.getCurrentWorkspace(
+              FakeAppFunctionContext(context)
+            )
+        assertThat(workspaceResponse.workspace).isEqualTo(fakeWorkspaceRepository.getWorkspace())
+        assertThat(workspaceResponse.proof).isEqualTo(Proof.GET_CURRENT_WORKSPACE_PROOF)
+    }
+
+    private class FakeWorkspaceRepository : WorkspaceRepository {
+        override suspend fun getWorkspace(): WorkspaceSpec {
+            return WorkspaceSpec(
+                screens = listOf(),
+                hotseat = HotseatSpec(listOf()),
+                rows = null,
+                columns = null,
+            )
+        }
+
+        override fun newTransaction(): WorkspaceTransaction {
+            return FakeWorkspaceTransaction()
+        }
+    }
+
+    private class FakeWorkspaceTransaction : WorkspaceTransaction {
+        override suspend fun commit(): WorkspaceSpec {
+            throw UnsupportedOperationException()
+        }
+    }
+
+    private class FakeAppFunctionContext(
+        override val context: Context,
+    ) : AppFunctionContext
+}
