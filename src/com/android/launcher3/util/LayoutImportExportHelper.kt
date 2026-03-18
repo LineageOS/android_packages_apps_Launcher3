05b23913b48ea0057961a7fc48e9be5626914707
Costin Brebu <costinbrebu@google.com>
2026-03-18 09:48:55 +0000

    Migrate LayoutImportExportHelper to use the ModelWriter for all database changes
    Fixed a possible small bug inside the BgDataModel. Removed items in the
    map after the database entries were deleted
    
    Bug: 491012579
    Test: Manual tested using the following import/export command
    adb shell content call --uri content://com.google.android.apps.nexuslauncher.settings --method EXPORT_LAYOUT_XML
    
    adb shell content call --uri content://com.google.android.apps.nexuslauncher.settings --method IMPORT_LAYOUT_XML --arg "<MY_XML>"
    FLAG: EXEMPT BUGFIX
    
    Change-Id: If56b8ad0253b70d0131c533bd1dac630a6c0cb10

--- src/com/android/launcher3/util/LayoutImportExportHelper.kt
+++ src/com/android/launcher3/util/LayoutImportExportHelper.kt
@@ -18,10 +18,8 @@ package com.android.launcher3.util
 import android.app.blob.BlobHandle.createWithSha256
 import android.app.blob.BlobStoreManager
 import android.content.Context
-import android.database.sqlite.SQLiteReadOnlyDatabaseException
 import android.os.ParcelFileDescriptor.AutoCloseOutputStream
 import android.provider.Settings.Secure
-import android.util.Log
 import com.android.launcher3.AutoInstallsLayout
 import com.android.launcher3.GridSizeUtil
 import com.android.launcher3.InvariantDeviceProfile
@@ -37,24 +35,32 @@ import com.android.launcher3.LauncherSettings.Settings.LAYOUT_DIGEST_LABEL
 import com.android.launcher3.LauncherSettings.Settings.LAYOUT_DIGEST_TAG
 import com.android.launcher3.LauncherSettings.Settings.LAYOUT_PROVIDER_KEY
 import com.android.launcher3.LauncherSettings.Settings.createBlobProviderKey
+import com.android.launcher3.celllayout.CellPosMapper
 import com.android.launcher3.concurrent.annotations.Background
 import com.android.launcher3.concurrent.annotations.Ui
 import com.android.launcher3.dagger.ApplicationContext
 import com.android.launcher3.logging.FileLog
+import com.android.launcher3.model.BgDataModel.ModificationSource
+import com.android.launcher3.model.ModelWriterFactory
 import com.android.launcher3.model.data.AppPairInfo
 import com.android.launcher3.model.data.FolderInfo
 import com.android.launcher3.model.data.ItemInfo
 import com.android.launcher3.model.data.LauncherAppWidgetInfo
+import com.android.launcher3.model.scheduleTransactionSuspending
 import com.android.launcher3.pm.UserCache
 import com.android.launcher3.shortcuts.ShortcutKey
-import com.android.launcher3.util.Executors.MODEL_EXECUTOR
 import com.android.users.UserType
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 import java.util.concurrent.CompletableFuture
-import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 
 class LayoutImportExportHelper
 @Inject
@@ -62,9 +68,18 @@ constructor(
     @ApplicationContext private val context: Context,
     private val model: LauncherModel,
     private val idp: InvariantDeviceProfile,
+    private val modelWriterFactory: ModelWriterFactory,
     @Background private val backgroundExecutor: ExecutorService,
     @Ui private val uiExecutor: ExecutorService,
 ) {
+
+    private val modelWriter =
+        modelWriterFactory.create(
+            verifyChanges = false,
+            cellPosMapper = CellPosMapper.DEFAULT,
+            modificationSource = ModificationSource.ModelTask,
+            owner = null,
+        )
 
     fun exportModelDbAsXmlFuture(): CompletableFuture<String> {
         val future = CompletableFuture<String>()
@@ -113,38 +128,39 @@ constructor(
      * data model. This will short-circuit (and not load the new data) if called without callbacks.
      */
     fun importModelFromXml(data: ByteArray) {
+        runBlocking {
+            withContext(backgroundExecutor.asCoroutineDispatcher()) {
+                importModelFromXmlSuspending(data)
+            }
+        }
+    }
+
+    /** Suspending version of [importModelFromXml]. */
+    suspend fun importModelFromXmlSuspending(data: ByteArray) {
         val digest = MessageDigest.getInstance("SHA-256").digest(data)
         val handle = createWithSha256(digest, LAYOUT_DIGEST_LABEL, 0, LAYOUT_DIGEST_TAG)
         val blobManager = context.getSystemService(BlobStoreManager::class.java)!!
-        val syncLatch = CountDownLatch(1)
 
         val resolver = context.contentResolver
         blobManager.openSession(blobManager.createSession(handle)).use { session ->
             AutoCloseOutputStream(session.openWrite(0, -1)).use { it.write(data) }
             session.allowPublicAccess()
-            session.commit(backgroundExecutor) {
-                Secure.putString(resolver, LAYOUT_PROVIDER_KEY, createBlobProviderKey(digest))
-                val xmlString = data.toString(StandardCharsets.UTF_8)
-                GridSizeUtil(context).parseAndSetGridSize(xmlString)
-                FileLog.i(TAG, "Importing XML as Home Screen Layout:\n$xmlString")
-                MODEL_EXECUTOR.submit { createEmptyDbSafe() }.get()
-                model.forceReload("importModelFromXml").toCompletableFuture().get()
-                uiExecutor.submit {}.get()
-                syncLatch.countDown()
+            suspendCancellableCoroutine { continuation ->
+                session.commit(backgroundExecutor) { continuation.resume(Unit) }
             }
         }
-        syncLatch.await()
-    }
 
-    private fun createEmptyDbSafe() {
-        try {
-            model.modelDbController.createEmptyDB()
-        } catch (e: SQLiteReadOnlyDatabaseException) {
-            // This issue has only been observed in tests so far, likely due
-            // to less strict threading for accessing and writing to the
-            // launcher test DB.
-            Log.w(TAG, "Failed to clear Launcher DB. It was already deleted.", e)
-        }
+        Secure.putString(resolver, LAYOUT_PROVIDER_KEY, createBlobProviderKey(digest))
+        val xmlString = data.toString(StandardCharsets.UTF_8)
+        GridSizeUtil(context).parseAndSetGridSize(xmlString)
+        FileLog.i(TAG, "Importing XML as Home Screen Layout:\n$xmlString")
+
+        modelWriter.scheduleTransactionSuspending { it.deleteAllItems() }
+
+        model.forceReload("importModelFromXml").await()
+
+        // Ensures that any pending UI tasks scheduled by forceReload are processed
+        withContext(uiExecutor.asCoroutineDispatcher()) {}
     }
 
     private fun LauncherLayoutBuilder.ItemTarget.addItem(context: Context, info: ItemInfo) {
